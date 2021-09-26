dbb16e56990e1d2325c8eb63ac1de927b05b792b
Brayan Yepes <abrayan.claye@gmail.com>
2021-09-26 00:26:26 +0000

    updating newSession.round

--- api/src/controllers/sessionscontroller.js
+++ api/src/controllers/sessionscontroller.js
@@ -1,3 +1,4 @@
+import questions from '../models/questionbank'
 import sessions  from '../models/sessions'
 
 export const createSessions = async (req, res) => {
@@ -8,4 +9,36 @@ export const createSessions = async (req, res) => {
     const newSession = new sessions({userId, prize:0, rounds:[]})
     const newSessionSaved = await newSession.save()
     return res.status(202).json({status: 202, data: {sessionId: newSessionSaved._id}})
+}
+
+export const patchSessions = (req, res) => {
+    const { userId, questionId, answerId } = req.body
+    if (userId === ""){
+        return res.status(411).json({status: 411, message:"userId field is required"})
+    }
+    if (questionId === ""){
+        return res.status(411).json({status: 411, message:"questionId field is required"})
+    }
+    if (answerId === ""){
+        return res.status(411).json({status: 411, message:"answerId field is required"})
+    }
+    const question = questions.find((element) => {
+        return element.id === questionId;
+      })
+    if (question.length === 0){
+        return res.status(404).json({status: 404, message:"question not found"})
+    }
+    const answer = question.options.find((element) => {
+        return element.id === answerId
+    })
+    if (answer.length === 0){
+        return res.status(404).json({status: 404, message:"answer not found"})
+    }
+    newSession.rounds = [
+        {
+            "question": questionId,
+            "answer": answerId,
+            "correct": answer.correct
+        }
+    ]
 }
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,7 @@ import * as questioncontroller from '../controllers/questioncontroller'
 import * as sessionscontroller from '../controllers/sessionscontroller'
 
 router.get('/questions/:categoryName', questioncontroller.getQuestion);
-router.post('/sessions', sessionscontroller.createSessions)
+router.post('/sessions', sessionscontroller.createSessions);
+router.patch('/sessions', sessionscontroller.patchSessions)
 
 export default router;
